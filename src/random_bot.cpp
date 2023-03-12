6eb5e5c81729ec23163a481b18f90523c3559e85
Olav Philipp Henschel <olav.ph@gmail.com>
2023-03-12 19:31:45 +0100

    Improve RandomBot
    Randomize within a delta of last value instead of full range.

--- src/random_bot.cpp
+++ src/random_bot.cpp
@@ -6,6 +6,13 @@
 using namespace std::chrono_literals;
 
 constexpr std::array ALLOWED_SYMBOLS{"AAPL", "GOOG", "META", "MSFT"};
+constexpr auto ORDER_INTERVAL = 100ms;
+constexpr price_t min_price = 1.0;
+constexpr price_t max_price = 100.0;
+constexpr price_t start_price = 50.0;
+constexpr price_t price_step_delta = 1.0;
+constexpr size_t min_volume = 1;
+constexpr size_t max_volume = 10;
 
 // Public
 
@@ -21,23 +28,24 @@ void RandomBot::run()
 
     thread = std::jthread([this](std::stop_token stop_token)
                           {
+        std::random_device rd;  // Will be used to obtain a seed for the random number engine
+        std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+        std::uniform_int_distribution<size_t> symbol_distrib(0, ALLOWED_SYMBOLS.size() - 1);
+        std::uniform_int_distribution<int> side_distrib(0, 1);
+        std::uniform_int_distribution<size_t> volume_distrib(min_volume, max_volume);
+
         order_id id = 1;
+        std::array<price_t, ALLOWED_SYMBOLS.size()> last_prices;
+        last_prices.fill(start_price);
+
         while (!stop_token.stop_requested())
         {
-            constexpr auto ORDER_INTERVAL = 1s;
-            constexpr price_t min_price = 1.0;
-            constexpr price_t max_price = 10.0;
-            constexpr size_t min_volume = 1;
-            constexpr size_t max_volume = 10;
+            auto symbol_index = symbol_distrib(gen);
+            std::uniform_real_distribution<price_t> price_distrib(
+                std::max(min_price, last_prices[symbol_index] - price_step_delta),
+                std::min(max_price, last_prices[symbol_index] + price_step_delta));
 
-            std::random_device rd;  // Will be used to obtain a seed for the random number engine
-            std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-            std::uniform_int_distribution<size_t> symbol_distrib(0, ALLOWED_SYMBOLS.size() - 1);
-            std::uniform_int_distribution<int> side_distrib(0, 1);
-            std::uniform_real_distribution<price_t> price_distrib(min_price, max_price);
-            std::uniform_int_distribution<size_t> volume_distrib(min_volume, max_volume);
-
-            auto symbol = ALLOWED_SYMBOLS[symbol_distrib(gen)];
+            auto symbol = ALLOWED_SYMBOLS[symbol_index];
             auto side = static_cast<Side>(side_distrib(gen));
             auto price = price_distrib(gen);
             auto volume = volume_distrib(gen);
@@ -47,6 +55,7 @@ void RandomBot::run()
 
             std::this_thread::sleep_for(ORDER_INTERVAL);
             ++id;
+            last_prices[symbol_index] = price;
         } });
 }
 
