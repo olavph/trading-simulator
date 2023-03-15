ae2c2256aab5aa731552030f9da8c6dd3955d6c5
Olav Philipp Henschel <olav.ph@gmail.com>
2023-03-15 17:47:07 +0100

    Fix deadlocks with MarketNotifier
    If an observer calls an agent that tries to create a new order during a
    price update, the MatchingEngine would dealock when calling the
    MarketNotifier again. By having its own thread, the MatchingEngine never
    needs to lock on the notifier's mutex.

--- src/observers/market_notifier.cpp
+++ src/observers/market_notifier.cpp
@@ -1,15 +1,21 @@
 #include "market_notifier.hpp"
 
 #include <algorithm>
+#include <iostream>
 
 // Public
 
-void MarketNotifier::subscribe(IMarketObserver * observer)
+MarketNotifier::MarketNotifier()
+{
+    run();
+}
+
+void MarketNotifier::subscribe(IMarketObserver *observer)
 {
     observers.push_back(observer);
 }
 
-void MarketNotifier::unsubscribe(IMarketObserver * observer)
+void MarketNotifier::unsubscribe(IMarketObserver *observer)
 {
     if (auto observer_it = std::find(observers.begin(), observers.end(), observer); observer_it != observers.end())
     {
@@ -19,16 +25,42 @@ void MarketNotifier::unsubscribe(IMarketObserver * observer)
 
 void MarketNotifier::orderAccepted(const NewOrder &order)
 {
-    for (auto &observer : observers)
-    {
-        observer->orderAccepted(order);
-    }
+    order_queue.push(order);
+    cv.notify_one();
 }
 
 void MarketNotifier::tradeExecuted(const Trade &trade)
 {
-    for (auto &observer : observers)
+    trade_queue.push(trade);
+    cv.notify_one();
+}
+
+// Private
+
+void MarketNotifier::run()
+{
+    thread = std::jthread([this](std::stop_token stop_token)
     {
-        observer->tradeExecuted(trade);
-    }
+        while (!stop_token.stop_requested())
+        {
+            std::unique_lock lock(mtx);
+            cv.wait(lock);
+            while (!order_queue.empty())
+            {
+                for (auto &observer : observers)
+                {
+                    observer->orderAccepted(order_queue.front());
+                }
+                order_queue.pop();
+            }
+            while (!trade_queue.empty())
+            {
+                for (auto &observer : observers)
+                {
+                    observer->tradeExecuted(trade_queue.front());
+                }
+                trade_queue.pop();
+            }
+        }
+    });
 }
--- src/observers/market_notifier.hpp
+++ src/observers/market_notifier.hpp
@@ -2,12 +2,18 @@
 
 #include "i_market_observer.hpp"
 
+#include <condition_variable>
+#include <queue>
+#include <thread>
 #include <memory>
+#include <mutex>
 #include <vector>
 
 class MarketNotifier : public IMarketObserver
 {
 public:
+    MarketNotifier();
+
     void subscribe(IMarketObserver *);
     void unsubscribe(IMarketObserver *);
 
@@ -15,5 +21,12 @@ public:
     void tradeExecuted(const Trade &) override;
 
 private:
+    void run();
+
     std::vector<IMarketObserver *> observers;
+    std::jthread thread;
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::queue<NewOrder> order_queue;
+    std::queue<Trade> trade_queue;
 };
